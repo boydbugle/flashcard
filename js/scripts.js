df85359731b1726ccc4561d0fdf4f9f044432cba
boyd dch <boydbugle@gmail.com>
2016-11-24 12:01:09 +0300

    1st trial toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,94 @@
 $(".jv").click(function() {
-    $("p").toggle();
+    $(".jv p").toggle();
+    $("h1.jv").toggle();
 });
+
 $(".op").click(function() {
-    $("p").toggle();
+    $(".op p").toggle();
+    $("h1.op").toggle();
+});
+
+$(".var").click(function() {
+    $(".var p").toggle();
+    $("h1.var").toggle();
+});
+
+$(".varNam").click(function() {
+    $(".varNam p").toggle();
+    $("h1.VarNam").toggle();
+});
+
+$(".fn").click(function() {
+    $(".fn p").toggle();
+    $("h1.fn").toggle();
+});
+
+$(".str").click(function() {
+    $(".str p").toggle();
+    $("h1.str").toggle();
+});
+
+$(".mthd").click(function() {
+    $(".mthd p").toggle();
+    $("h1.mthd").toggle();
+});
+
+$(".arg").click(function() {
+    $(".arg p").toggle();
+    $("h1.arg").toggle();
+});
+
+$(".par").click(function() {
+    $(".par p").toggle();
+    $("h1.par").toggle();
+});
+
+$(".rtrn").click(function() {
+    $(".rtrn p").toggle();
+    $("h1.rtrn").toggle();
+});
+
+$(".chnMthd").click(function() {
+    $(".chnMthd p").toggle();
+    $("h1.chnMthd").toggle();
+});
+
+$(".bolean").click(function() {
+    $(".bolean p").toggle();
+    $("h1.bolean").toggle();
+});
+
+$(".undfnd").click(function() {
+    $(".undfnd p").toggle();
+    $("h1.undfnd").toggle();
+});
+
+$(".NaN").click(function() {
+    $(".NaN p").toggle();
+    $("h1.NaN").toggle();
+});
+
+$(".esc").click(function() {
+    $(".esc p").toggle();
+    $("h1.esc").toggle();
+});
+
+$(".alert").click(function() {
+    $(".alert p").toggle();
+    $("h1.alert").toggle();
+});
+
+$(".comments").click(function() {
+    $(".comments p").fadeToggle();
+    $("h1.comments").fadeToggle();
+});
+
+$(".jQuery").click(function() {
+    $(".jQuery p").slideToggle();
+    $("h1.jQuery").slideToggle();
+});
+
+$(".attrbt").click(function() {
+    $(".attrbt p").toggle();
+    $("h1.attrbt").toggle();
 });
